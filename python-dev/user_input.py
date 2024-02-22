3b3526617df021eab485dda003910a70f01460e2
dkomics <klay.py@gmail.com>
2024-02-23 01:55:25 +0300

    added a file user_input.py

---
+++ python-dev/user_input.py
@@ -0,0 +1,13 @@
+# This program prompts a user to input his/her infromation
+
+# Ask the user for their name and store it in the "name" variable
+name = input("What is your name? ")
+
+# Ask the user for their age and store it in the "age" variable
+age = input("How old are you? ")
+
+# Ask the user for their location and store it in the "location" variable
+location = input("Where do you live? ")
+
+# Print out a personalized message using the user's information
+print(f"Hello {name}, you are {age} years old and live in {location}.")
